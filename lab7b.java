46ced132eab2050f467abd6bec10faf6bcb7c49e
Sinchana2250 <sinchana.23cs158@sode-edu.in>
2025-10-03 16:25:44 +0530

    Update lab7b.java

--- lab7b.java
+++ lab7b.java
@@ -1,26 +1,29 @@
 package com.smvitm.cnlab;
 
 import java.net.*;
+import java.util.*;
 
-public class DatagramSocketClient {
+public class DatagramSocketserver {
 
     public static void main(String[] args) throws Exception {
-        String line = "connected with client";
-        DatagramSocket clientSocket = new DatagramSocket();
-        InetAddress IPAddress = InetAddress.getByName("localhost");
-        byte[] sendData = new byte[1024];
+        Scanner in = new Scanner(System.in);
+        DatagramSocket serverSocket = new DatagramSocket(9000);
         byte[] receiveData = new byte[1024];
-        sendData = line.getBytes();
-        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9000);
-        clientSocket.send(sendPacket);
-        System.out.println("****Client Display Terminal***");
+        byte[] sendData = new byte[1024];
+        System.out.println("***Server side***");
+        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+        serverSocket.receive(receivePacket);
+        System.out.println(new String(receivePacket.getData()));
+        InetAddress IPAddress = receivePacket.getAddress();
+        int port = receivePacket.getPort();
         while (true) {
-            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-            clientSocket.receive(receivePacket);
-            String messageReceived = new String(receivePacket.getData(), receivePacket.getOffset(), receivePacket.getLength());
-            System.out.println("message typed at server side is:" + messageReceived);
-
+            System.out.println("Type some message to display at client end");
+            String message = in.nextLine();
+            sendData = message.getBytes();
+            System.out.println("message sent from the server " + new String(sendData));
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+            serverSocket.send(sendPacket);
         }
-
     }
+
 }
